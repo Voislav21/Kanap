aa35251dce7410ddd340537ca7c5ca4724c0e8a4
Voislav Ivanovski <voislav21@gmail.com>
2023-03-30 15:40:25 +0200

    Add type=module to script tag. Modified script.js code and added comments

--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,21 @@
-const url = "http://localhost:3000/api/products/";
-const cards = document.querySelector("#items");
+//Retrive data from API//
+const response = await fetch('http://localhost:3000/api/products/');
+const items = await response.json();
 
+console.log(items);
+
+//Create function that will sort through the data to populate the cards//
 const getArticles = () => {
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        for(product in data){
-            cards.innerHTML += `<a href="./product.html?id=42">
-            <article>
-              <img src="${data[product].imageUrl}" alt="${data[product].altTxt}">
-              <h3 class="productName">${data[product].name}</h3>
-              <p class="productDescription">${data[product].description}</p>
-            </article>`
-        }
-    })
+    for (let i=0; i<items.length;i++) {
+        const cards = document.querySelector("#items");
+        cards.innerHTML += `<a href="./product.html?id=${items[i]._id}">
+        <article>
+          <img src="${items[i].imageUrl}" alt="${items[i].altTxt}">
+          <h3 class="productName">${items[i].name}</h3>
+          <p class="productDescription">${items[i].description}</p>
+        </article>
+        </a>`
+    }
 }
-
+//Call the function//
 getArticles();
