697f5cdf8ef373de21e24698e1cadbd71064d67a
Voislav Ivanovski <voislav21@gmail.com>
2023-04-01 11:42:55 +0200

    Changed function to async await which helped with lag on home page. Added comments to both js files

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
-//Retrive data from API//
+//Retrive only one items data from URL using its id//
 const id = new URLSearchParams(window.location.search).get("id");
 console.log(id);
 
+//Create function to populate the product page specific to its id//
 const getArticle  = () => {
     fetch(`http://localhost:3000/api/products/${id}`)
     .then(res => {
@@ -12,11 +13,11 @@ const getArticle  = () => {
         const addImg = document.querySelector(".item__img");
         addImg.innerHTML += `<img src="${item.imageUrl}" alt="${item.altTxt}">`;
         const addTitle = document.querySelector("#title");
-        addTitle.innerHTML = item.name;
+        addTitle.textContent = item.name;
         const addPrice = document.querySelector("#price");
-        addPrice.innerHTML = item.price;
+        addPrice.textContent = item.price;
         const addDescription = document.querySelector("#description");
-        addDescription.innerHTML = item.description;
+        addDescription.textContent = item.description;
 
         const colorOption = document.querySelector("#colors");
         for (i in item.colors) {
@@ -25,5 +26,5 @@ const getArticle  = () => {
         console.log(colorOption)
     })
 }
-
+//Call the function//
 getArticle();
--- front/js/script.js
+++ front/js/script.js
@@ -1,27 +1,6 @@
-//Retrive data from API//
-//const response = await fetch('http://localhost:3000/api/products/');
-//const items = await response.json();
-//
-//console.log(items);
-//
-////Create function that will sort through the data to populate the cards//
-//const getArticles = () => {
-//    for (let i=0; i<items.length;i++) {
-//        const cards = document.querySelector("#items");
-//        cards.innerHTML += `<a href="./product.html?id=${items[i]._id}">
-//        <article>
-//          <img src="${items[i].imageUrl}" alt="${items[i].altTxt}">
-//          <h3 class="productName">${items[i].name}</h3>
-//          <p class="productDescription">${items[i].description}</p>
-//        </article>
-//        </a>`
-//    }
-//}
-////Call the function//
-//getArticles();
-
-const getArticles = () => {
-    fetch("http://localhost:3000/api/products")
+//Create function that will sort through the data to populate the cards//
+async function getArticles () {
+    await fetch("http://localhost:3000/api/products")
     .then(data => {
         return data.json();
     })
@@ -40,4 +19,5 @@ const getArticles = () => {
         console.log(cards)
     })
 }
+//Call the function//
 getArticles();
