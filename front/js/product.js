22462adcbe2ea603f027fc942796018a4c11695a
Voislav Ivanovski <voislav21@gmail.com>
2023-04-03 19:47:28 +0200

    updated the event listener and finished the code that will add the products to the local storage

--- front/js/product.js
+++ front/js/product.js
@@ -30,12 +30,41 @@ const getArticle  = () => {
 
 const addToCart = document.querySelector("#addToCart");
 addToCart.addEventListener('click', () => {
+    const colorSelect = document.querySelector("#colors").value;
+    const quantitySelect = document.querySelector("#quantity").value;
+
+    //if the color or the quantity is not selected display an error message and prevent the code from executing//
+    if(colorSelect === '' || quantitySelect === '' || parseInt(quantitySelect) <= 0 || parseInt(quantitySelect) > 100) {
+        alert ("Please select a color and a positive number between 1-100!");
+        return;
+    }
+
+    //Information to be pushed to cart//
     const addProduct = {
         id: id,
-        color: document.querySelector("#colors").value,
-        quantity: document.querySelector("#quantity").value
+        color: colorSelect,
+        quantity: quantitySelect
     }
-    console.log(addProduct);
+    //Get the current cart items or create an empty array//
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    //Find existing product with the same id and color//
+    let existingProduct = cart.find(item => item.id === addProduct.id && item.color === addProduct.color);
+
+    if (existingProduct) {
+        //if existing product is found, update its quantity//
+        existingProduct.quantity = parseInt(existingProduct.quantity) + parseInt(addProduct.quantity);
+        if(existingProduct.quantity > 100) {
+            alert(`You can not order more than 100 of the same product. You currently have: ${existingProduct.quantity}`);
+            return;
+        }
+        alert(`Added ${quantitySelect} of the ${colorSelect} couch to your cart!`);
+    } else {
+        //if no existing product add product to cart//
+        alert(`Added ${quantitySelect} of the ${colorSelect} couch to your cart!`);
+        cart.push(addProduct);
+    }
+    //store array into local storage//
+    localStorage.setItem("cart", JSON.stringify(cart));
 });
 
 
